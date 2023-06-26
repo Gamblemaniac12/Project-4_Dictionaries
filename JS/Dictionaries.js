2cef3c52fb4084ddab92c360410dd239976ca33b
Gamblemaniac12 <calebgambleman@gmail.com>
2023-06-25 23:50:26 -0700

    added all

---
+++ JS/Dictionaries.js
@@ -0,0 +1,10 @@
+function my_Dictionaries() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionaries").innerHTML = Animal.Sound;
+}
